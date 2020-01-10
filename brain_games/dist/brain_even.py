514ef5e4092d8edcf3caa4116f180b1238435c4f
postscriptumno <postscriptum.no@gmail.com>
2020-01-10 14:33:04 +0200

    Wrote logic of brain-even

--- brain_games/dist/brain_even.py
+++ brain_games/dist/brain_even.py
@@ -1,8 +1,22 @@
 import prompt
+import random
 
 
 def run():
     print('Answer "yes" if number even otherwise answer "no".\n')
     name = prompt.string('May I have your name? ')
     print('\nHello, {}!'.format(name))
+
+    for i in range(3):
+        num = random.randint(1, 100)
+        print('\nQuestion: {}'.format(num))
+        correct_answer = 'yes' if num % 2 == 0 else 'no'
+        answer = prompt.string('Your answer: ')
+        if correct_answer == answer:
+            print('Correct!')
+        else:
+            print('\n\"{}\" is wrong answer ;(. Correct answer was \"{}\"'.format(answer, correct_answer))
+            print('Let\'s try again, {}'.format(name))
+            return
+    print('\nCongratulations, {}'.format(name))
     return
