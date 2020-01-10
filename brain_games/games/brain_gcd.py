b72bcac33dc5c8a13868e84af8b4bf9fee900ac6
postscriptumno <postscriptum.no@gmail.com>
2020-01-10 22:12:28 +0200

    Added brain-gcd logic, script, asciinema

---
+++ brain_games/games/brain_gcd.py
@@ -0,0 +1,26 @@
+import prompt
+import random
+from math import gcd
+
+
+def run():
+    print('Welcome to the Brain Games!')
+    print('What is the result of the expression?\n')
+    name = prompt.string('May I have your name? ')
+    print('\nHello, {}!\n'.format(name))
+
+    for i in range(3):
+        num1 = random.randint(0, 10)
+        num2 = random.randint(0, 10)
+        print('Question: {} {}'.format(num1, num2))
+        user_answer = prompt.integer('Your answer: ')
+        correct_answer = gcd(num1, num2)
+        if user_answer == correct_answer:
+            print('Correct!')
+            continue
+        else:
+            result = '\"{}\" is wrong answer ;(. Correct answer was \"{}\"'
+            print(result.format(user_answer, correct_answer))
+            return
+    print('Congratulations, {}'.format(name))
+    return
